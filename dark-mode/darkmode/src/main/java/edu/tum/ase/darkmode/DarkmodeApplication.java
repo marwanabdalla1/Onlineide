ea661a368961a5d0c2eff423e715053281910369
marwanabdalla1 <marwanabdalla659@gmail.com>
2023-11-15 20:05:20 +0100

    worked on ex3

--- dark-mode/darkmode/src/main/java/edu/tum/ase/darkmode/DarkmodeApplication.java
+++ dark-mode/darkmode/src/main/java/edu/tum/ase/darkmode/DarkmodeApplication.java
@@ -10,10 +10,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication @RestController
 public class DarkmodeApplication {
+  boolean darkMode = false;
+
+      // modify this to implement a button which toggles the dark mode
+
 public static void main(String[] args) { SpringApplication.run(DarkmodeApplication.class, args);
 }
-@RequestMapping(path = "/dark-mode/toggle", method = RequestMethod.GET) public void toggleDarkMode() {
+@RequestMapping(path = "/dark-mode/toggle", method = RequestMethod.GET) public String toggleDarkMode() {
     // TODO: implement dark mode toggle
+
+
+
+
+    
+    // wait for three seconds before toggling
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+   darkMode = !darkMode;
+
+   return "darkmode is " + (darkMode ? "enabled" : "disabled");
+
+}
+
+
+@RequestMapping(path = "/dark-mode/status", method = RequestMethod.GET) public String getDarkModeStatus() {
+  
+  return "darkmode currently is " + (darkMode ? "enabled" : "disabled");
 }
   // TODO: implement endpoint which returns dark mode status
 }
