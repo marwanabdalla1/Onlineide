3100629aade9ef8e4e32076089651a2ca2847cc7
marwanabdalla1 <marwanabdalla659@gmail.com>
2023-11-15 10:32:21 +0100

    Added the DarkmodeApplication starting code

--- dark-mode/darkmode/src/main/java/edu/tum/ase/darkmode/DarkmodeApplication.java
+++ dark-mode/darkmode/src/main/java/edu/tum/ase/darkmode/DarkmodeApplication.java
@@ -1,13 +1,19 @@
+
+
+
 package edu.tum.ase.darkmode;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; import org.springframework.web.bind.annotation.RequestMapping; import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+
+@SpringBootApplication @RestController
 public class DarkmodeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DarkmodeApplication.class, args);
-	}
-
+public static void main(String[] args) { SpringApplication.run(DarkmodeApplication.class, args);
+}
+@RequestMapping(path = "/dark-mode/toggle", method = RequestMethod.GET) public void toggleDarkMode() {
+    // TODO: implement dark mode toggle
+}
+  // TODO: implement endpoint which returns dark mode status
 }
